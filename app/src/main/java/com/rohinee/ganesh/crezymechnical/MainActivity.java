0f82b73ab01c8de62ad88c45ba72b76f08b73663
Rohinee-Misal <rohineemisal92@gmail.om>
2018-06-28 22:18:46 +0530

    latest git

--- app/src/main/java/com/rohinee/ganesh/crezymechnical/MainActivity.java
+++ app/src/main/java/com/rohinee/ganesh/crezymechnical/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("hie","2");
+        Log.i("hie","");
     }
 }
